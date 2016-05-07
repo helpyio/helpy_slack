ed19c054f519bf3c94fdae4b866488bec1262234
Scott Miller <scott.miller.utah@gmail.com>
2016-05-06 21:28:02 -0600

    Add attachment and formatting

--- app/models/topic_decorator.rb
+++ app/models/topic_decorator.rb
@@ -2,13 +2,20 @@ Topic.class_eval do
 
   after_create :ping_slack
 
-  protected
+  # protected
 
   def ping_slack
     notifier = Slack::Notifier.new 'https://hooks.slack.com/services/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'
     notifier.username = "HelpyBot"
+
     url = "#{AppSettings['settings.site_url']}#{Rails.application.routes.url_helpers.admin_ticket_path(self.id)}"
-    notifier.ping "New Topic: <a href='#{url}'>#{self.name}</a> was just posted by #{self.user.name}"
+    message = {
+      title: "<#{url}|#{self.name}>",
+      text: self.posts.first.body,
+      color: '#eee'
+    }
+
+    notifier.ping "New Topic started by #{self.user.name}", attachments: [message]
   end
 
 end
