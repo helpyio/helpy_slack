f0365edac17cabe05aaf3422c3661ee851fd6ed9
scott <scott.miller.utah@gmail.com>
2017-05-19 09:47:24 -0600

    Add username back in correct place

--- app/jobs/slack_job.rb
+++ app/jobs/slack_job.rb
@@ -1,5 +1,5 @@
 class SlackJob < ActiveJob::Base
-  
+
   def perform(post)
     # Assemble message
     url = "#{AppSettings['settings.site_url']}#{Rails.application.routes.url_helpers.admin_topic_path(post.topic.id)}"
@@ -11,7 +11,7 @@ class SlackJob < ActiveJob::Base
     }
 
     # Send Ping
-    notifier = Slack::Notifier.new AppSettings['slack.post_url']
+    notifier = Slack::Notifier.new AppSettings['slack.post_url'], username: "HelpyBot"
     notifier.ping title, attachments: [message], channel: AppSettings['slack.default_channel']
   end
 end
